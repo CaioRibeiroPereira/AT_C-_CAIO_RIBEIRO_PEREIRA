4a1bcb2de4d2b746f21e2928e139d0e2be3b0f7a
CAIO PEREIRA <ribeirocaio146@gmail.com>
2025-04-01 20:39:25 -0300

    Add files via upload

---
+++ EXERCICIO 01/EXERCICIO_01_AT_C#_CAIO_RIBEIRO/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Olá, meu nome é Caio Ribeiro Pereira!");
+        Console.WriteLine("Nasci em 03 de setembro de 2004 e estou aprendendo C#!");
+    }
+}
